b2de7eb12d6c17c71d52cbf1a2beb9e2de93ffb2
galdinojr <galdinodbz@gmail.com>
2019-01-24 00:19:18 -0200

    ajuste de layout no login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login2', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login2', loadChildren: './login2/login2.module#Login2PageModule' },
 ];
 
 @NgModule({
