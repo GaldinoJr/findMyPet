e07e6ca3f0f565843223df264c68f905b66db15b
galdinojr <galdinodbz@gmail.com>
2019-01-29 17:30:57 -0200

    comeco do desenolvimento da lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login2', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'login2', loadChildren: './login2/login2.module#Login2PageModule' },
