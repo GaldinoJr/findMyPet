99bf043e0bdb8e3ea10f1d14f18b3d8725a44cbf
galdinojr <galdinodbz@gmail.com>
2019-02-05 18:32:44 -0200

    melhorias no layout do detalhe

--- src/app/pages/dog-detail/dog-detail.page.ts
+++ src/app/pages/dog-detail/dog-detail.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Dog } from 'src/domain/home/dog';
 import { ActivatedRoute } from '@angular/router';
 import { DogService } from 'src/services/dogService';
+import { isNullOrUndefined } from 'util';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dog-detail',
@@ -9,15 +11,32 @@ import { DogService } from 'src/services/dogService';
   styleUrls: ['./dog-detail.page.scss'],
 })
 export class DogDetailPage implements OnInit {
-  dog: Dog;
+  dog: Dog = new Dog;
+  form = new FormGroup({
+    breed: new FormControl(this.dog.breed, Validators.required),
+    date: new FormControl(this.dog.daysGone, Validators.required),
+    local: new FormControl(this.dog.streetLost, Validators.required),
+    name: new FormControl('', Validators.required),
+    phoneNumber: new FormControl('', Validators.required),
+  });
 
   constructor(private route: ActivatedRoute, dogService: DogService) {
-    // let id = this.route.snapshot.paramMap.get("id");
-    // this.dog = dogService.getDogs(parseInt(id));
-    // window.alert(this.dog.name)
+    let id = this.route.snapshot.paramMap.get("id");
+    if(isNullOrUndefined(id)){
+      
+    }
+    else{
+      this.dog = dogService.getDogs(parseInt(id));
+      
+    }
+    
    }
 
   ngOnInit() {
+    
+  }
+
+  onSaveClicked(){
     
   }
 
